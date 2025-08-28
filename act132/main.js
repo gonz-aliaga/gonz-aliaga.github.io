6a0ff1ce88d77c163f67a5b7a872a31d7765e7f0
gonz-aliaga <gonz.aliaga@duocuc.cl>
2025-08-28 10:41:23 -0400

    Pruebas de JavaScript básicas, form e innerHTML

---
+++ act132/main.js
@@ -0,0 +1,28 @@
+function getData(form){
+    // Conseguir los datos del formulario
+    var formData = new FormData(form);
+    // Formatear los datos a objeto JS
+    var data = Object.fromEntries(formData);
+
+    // Mostrar en la consola los datos
+    console.log(data)
+    console.log(JSON.stringify(data, null, 4));
+
+    return data;
+}
+
+document.getElementById("myForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    const datos = getData(e.target);
+
+    if (datos.age < 18) {
+        document.getElementById("output").innerHTML =
+            `<p>¡Hola, ${datos.name}! Eres menor de edad.</p>`;
+    } else if (65 > datos.age && datos.age >= 18) {
+        document.getElementById("output").innerHTML =
+            `<p>¡Hola, ${datos.name}! Eres mayor de edad.</p>`;
+    } else {
+        document.getElementById("output").innerHTML =
+            `<p>¡Hola, ${datos.name}! Eres de edad avanzada.</p>`;
+    }
+});;
