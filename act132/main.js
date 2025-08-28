f01f76854ca6395eb0682f967b6d3a0e454ae6ae
gonz-aliaga <gonz.aliaga@duocuc.cl>
2025-08-28 11:12:07 -0400

    Mejora en flujo de condiciones

--- act132/main.js
+++ act132/main.js
@@ -18,11 +18,14 @@ document.getElementById("myForm").addEventListener("submit", function(e) {
     if (datos.age < 18) {
         document.getElementById("output").innerHTML =
             `<p>¡Hola, ${datos.name}! Eres menor de edad.</p>`;
-    } else if (65 > datos.age && datos.age >= 18) {
+    } else if (65 >= datos.age && datos.age >= 18) {
         document.getElementById("output").innerHTML =
             `<p>¡Hola, ${datos.name}! Eres mayor de edad.</p>`;
-    } else {
+    } else if (65 < datos.age) {
         document.getElementById("output").innerHTML =
             `<p>¡Hola, ${datos.name}! Eres de edad avanzada.</p>`;
+    } else {
+        document.getElementById("output").innerHTML =
+        `<p>¡Por favor ingresa una edad válida!</p>`
     }
 });;
